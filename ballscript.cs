4d8392c96d5f9723afa79a4b9881741612dc8044
Yogi nadianto <62419479+yogi-nadianto@users.noreply.github.com>
2020-04-15 16:32:03 +0300

    Add files via upload

---
+++ ballscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballscript : MonoBehaviour
+{
+    public int speed=30;
+    public Rigidbody2D sesuatu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Muhammad Fahreza Alfa Sina Mustofa");
+        sesuatu.velocity = new Vector2(1,-1)*speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.name=="Kanan" || other.collider.name=="Kiri") {
+            GetComponent<Transform>().position = new Vector2(0,0);    
+        }
+    }
+}
